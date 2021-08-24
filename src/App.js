677519ae421b246f5c1aa26271ce7d52d48989fb
DmitriyGloyov <dglotov.exceedteam@gmail.com>
2021-08-24 16:13:54 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
 
-
-
-const searchPanel = <input type="text" className="search-input" placeholder="type to search" />
-
-const buttonAdd = <button type="button" textContent="Add">Add</button>
-
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        {searchPanel}
-        {buttonAdd}
+      <input type="text" className="search-input" placeholder="type to search" />
+      <button type="button" textContent="Add">Add</button>
       </header>
     </div>
   );
