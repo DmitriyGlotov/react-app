a2e8a6f6b50411eab421c8d2e95c633a72050247
DmitriyGloyov <dglotov.exceedteam@gmail.com>
2021-08-25 09:09:44 +0300

    fixed part2

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-      <input type="text" className="search-input" placeholder="type to search" />
-      <button type="button" textContent="Add">Add</button>
+        <input type="text" className="search-input" placeholder="type to search" />
+        <button type="button" textContent="Add">Add</button>
       </header>
     </div>
   );
